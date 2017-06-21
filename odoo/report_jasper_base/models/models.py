6296cbaa02595c43c4e3ea9dd2e5317cbe318753
Fabio Negrini <fnegrini@gmail.com>
2017-06-21 11:33:59 -0300

    Odoo module: main report and subreports fields

--- odoo/report_jasper_base/models/models.py
+++ odoo/report_jasper_base/models/models.py
@@ -6,6 +6,7 @@ class jasper_report(models.Model):
     _inherit = 'ir.actions.report.xml'
 
     report_type = fields.Selection(selection_add=[('jasper','Jasper Report')])
+    
     jasper_output_type = fields.Selection([("PDF", " PDF"),\
                                            ("HTML", "HTML"),\
                                            ("CSV", "CSV"),\
@@ -13,5 +14,22 @@ class jasper_report(models.Model):
                                            ("TEXT", "Plain text"),\
                                            ("XML", "XML")],\
                                             string = 'Output format')
+    
     jasper_jrxml_file = fields.Binary(string='Design file', filters='*.jrxml')
+    
     jasper_jasper_file = fields.Binary(string='Compiled file', filters='*.jasper')
+    
+    sub_reports = fields.One2many('jasper.sub.report', 'report', string="Subreports") 
+
+
+class jasper_sub_report(models.Model):
+    _name = 'jasper.sub.report'
+    
+    report = fields.Many2one('ir.actions.report.xml', string="Report", index=True, ondelete='cascade', required=True)
+    
+    param_name = fields.Char(String='Param Name', size=30, required=True, translate=False)
+    
+    jasper_jrxml_file = fields.Binary(string='Design file', filters='*.jrxml')
+    
+    jasper_jasper_file = fields.Binary(string='Compiled file', filters='*.jasper')
+    
